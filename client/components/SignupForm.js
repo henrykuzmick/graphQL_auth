7447ad67619d9a485dbeffade8c3aa9ef089ed5d
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-10-14 20:20:30 +0200

    Auth Forms Redirect

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 
 import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
 import AuthForm from './AuthForm';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: []
+    };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
+  }
+
   onSubmit({ email, password }) {
     this.props.mutate({
-      variables: { email, password }
-    });
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors })
+    })
   }
 
   render() {
@@ -17,11 +36,13 @@ class SignupForm extends Component {
         <h3>Sign Up</h3>
         <AuthForm
           onSubmit={this.onSubmit.bind(this)}
-          errors={[]}
+          errors={this.state.errors}
         />
       </div>
     )
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+)
