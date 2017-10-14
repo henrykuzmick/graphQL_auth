76b2df46070e1da3fe86549a45a9b829f54b2be5
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-10-14 17:07:44 +0200

    define signup and logout mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -4,6 +4,7 @@ const {
   GraphQLString
 } = graphql;
 const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -14,8 +15,16 @@ const mutation = new GraphQLObjectType({
         email: { type: GraphQLString },
         password: { type: GraphQLString }
       },
-      resolve(parentValue, args, request) {
-        
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req })
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
       }
     }
   }
