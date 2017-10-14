2fff3989aa1e1db6797a0cad8414b9252cd93508
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-10-14 20:36:41 +0200

    Add requireAuth HOC

--- client/index.js
+++ client/index.js
@@ -7,6 +7,8 @@ import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
+import requireAuth from './components/requireAuth';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -25,6 +27,7 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path='/' component={App}>
+          <Route path='dashboard' component={requireAuth(Dashboard)} />
           <Route path='login' component={LoginForm} />
           <Route path='signup' component={SignupForm} />
         </Route>
